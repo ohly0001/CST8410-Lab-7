0e3d7c452ed95a933ef8a29d221a4a00a7bc7677
Robert Ohly <ohly0001@algonquinlive.com>
2025-10-16 15:32:14 -0400

    Added chat Entry

--- app/src/main/java/com/example/lab7/MainActivity.kt
+++ app/src/main/java/com/example/lab7/MainActivity.kt
@@ -4,44 +4,63 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lab7.ui.theme.Lab7Theme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab7Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Surface(modifier = Modifier.fillMaxSize()) {
+                ChatWindow()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ChatEntry(mirror: Boolean, pfpId: Int, message: String, sentOn: Instant) {
+    val timestamp = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd HH:mm:ss")
+        .withZone(ZoneOffset.UTC)
+        .format(sentOn)
+
+    val elements = listOf<@Composable () -> Unit>(
+        { Image(painterResource(pfpId), "Profile Picture") },
+        {
+            Column {
+                Text(message)
+                Text(timestamp, style = MaterialTheme.typography.labelSmall)
+            }
+        }
     )
+
+    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        if (!mirror) {
+            elements.forEach { it() }
+        } else {
+            elements.reversed().forEach { it() }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab7Theme {
-        Greeting("Android")
+fun ChatWindow() {
+    Column {
+
     }
 }
